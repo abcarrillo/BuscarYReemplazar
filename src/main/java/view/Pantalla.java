f3c6e7c949a7c047ca37475a55fcb4df82c862c5
abcarrillo <albertobarrosocarrillo@gmail.com>
2020-11-01 13:10:54 +0000

    Update Pantalla.java

--- src/main/java/view/Pantalla.java
+++ src/main/java/view/Pantalla.java
@@ -3,6 +3,7 @@ package view;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.control.Button;
+import javafx.scene.control.CheckBox;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.GridPane;
@@ -15,9 +16,11 @@ public class Pantalla extends GridPane{
 		
 		prefHeight(Double.MAX_VALUE);
 		prefWidth(Double.MAX_VALUE);
-		setGridLinesVisible(true);
+		//setGridLinesVisible(true);
+		int widthBtn = 100;
+		int checkWidth = 130;
 		
-		setHgap(10);
+		setHgap(8);
 		setVgap(5);
 		setPadding(new Insets(10, 10, 10, 10));
 		VBox panel1 = new VBox();
@@ -32,21 +35,45 @@ public class Pantalla extends GridPane{
 		TextField buscarText = new TextField();
 		buscarText.setPrefWidth(250);
 		Button buscarBtn = new Button("Buscar");
-		//buscarBtn.setPrefWidth(110);
+		buscarBtn.setPrefWidth(widthBtn);
+		Label reemplazar = new Label("Reemplazar con: ");
+		reemplazar.setPrefWidth(100);
+		TextField reemplazarText = new TextField();
+		reemplazarText.setPrefWidth(250);
+		Button reemplazarBtn = new Button("Reemplazar");
+		reemplazarBtn.setPrefWidth(widthBtn);
+		Button todo = new Button("Reemplazar todo");
+		todo.setPrefWidth(widthBtn);
+		Button cerrarBtn = new Button("Cerrar");
+		cerrarBtn.setPrefWidth(widthBtn);
+		Button ayudaBtn = new Button("Ayuda");
+		ayudaBtn.setPrefWidth(widthBtn);
+		CheckBox check1 = new CheckBox("Mayúsculas y minúsculas");
+		check1.setPrefWidth(checkWidth);
+		CheckBox check2 = new CheckBox("Buscar hacia atrás");
+		check2.setPrefWidth(checkWidth);
+		CheckBox check3 = new CheckBox("Expresión regular");
+		check3.setPrefWidth(checkWidth);
+		CheckBox check4 = new CheckBox("Resaltar resultados");
+		check4.setPrefWidth(checkWidth);
 		
 		HBox cajaBuscar = new HBox();
 		cajaBuscar.setAlignment(Pos.CENTER);
 		cajaBuscar.getChildren().addAll(buscar, buscarText);
 		
-		Label reemplazar = new Label("Reemplazar con: ");
-		TextField reemplazarText = new TextField();
-		Button reemplazarBtn = new Button("Reemplazar");
-		Button todo = new Button("Reemplazar todo");
-		Button cerrarBtn = new Button("Cerrar");
-		Button ayudaBtn = new Button("Ayuda");
+		HBox cajaReemplazar = new HBox();
+		cajaReemplazar.setAlignment(Pos.CENTER);
+		cajaReemplazar.getChildren().addAll(reemplazar, reemplazarText);
+		
+		HBox cajaCheckbox = new HBox();
+		cajaCheckbox.setAlignment(Pos.CENTER);
+		cajaCheckbox.getChildren().addAll(check1, check2);
+		HBox cajaCheckbox2 = new HBox();
+		cajaCheckbox2.setAlignment(Pos.CENTER);
+		cajaCheckbox2.getChildren().addAll(check3, check4);
 
-		panel1.getChildren().addAll(cajaBuscar);
-		panel2.getChildren().addAll(buscarBtn);
+		panel1.getChildren().addAll(cajaBuscar, cajaReemplazar, cajaCheckbox, cajaCheckbox2);
+		panel2.getChildren().addAll(buscarBtn, reemplazarBtn, todo, cerrarBtn, ayudaBtn);
 		
 		add(panel1, 0, 0, 10, 1);
 		add(panel2, 11, 0, 2, 1);
